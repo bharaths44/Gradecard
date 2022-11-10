9f55354aa6515d3486fcb478761cbb2815576cda
Bharath S <94459111+brion4@users.noreply.github.com>
2022-11-10 15:39:12 +0530

    Add files via upload

--- gradecard.cpp
+++ gradecard.cpp
@@ -7,9 +7,9 @@ class student{
 	string stdName;
 	void getpersonalDetails()
 	{
-		cout<<"Enter Student ID :"<<endl;
+		cout<<"Enter Student ID :";
 		cin>>stdID;
-		cout<<"Enter student name :"<<endl;
+		cout<<"Enter student name :";
 		cin>>stdName;
 	}
 	void putpersonalDetails()
@@ -26,9 +26,9 @@ class teacher{
 	string trName;
 	void getpersonalDetails()
 	{
-		cout<<"Enter teacher ID :"<<endl;
+		cout<<"Enter teacher ID :";
 		cin>>trID;
-		cout<<"Enter teacher name :"<<endl;
+		cout<<"Enter teacher name :";
 		cin>>trName;
 	}
 	void putpersonalDetails()
@@ -45,20 +45,20 @@ class gradeCard{
 	float gpa;
 	void enterMarks()
 	{
-		cout<<"Enter student ID :"<<endl;
+		cout<<"Enter student ID :";
 		cin>>stdID;
-		cout<<"Enter teacher ID :"<<endl;
+		cout<<"Enter teacher ID :";
 		cin>>trID;
-		cout<<"Enter internal marks 1:"<<endl;
+		cout<<"Enter internal marks 1:";
 		cin>>intm1;
-		cout<<"Enter internal marks 2:"<<endl;
+		cout<<"Enter internal marks 2:";
 		cin>>intm2;
-		cout<<"Enter asmark1 :"<<endl;
+		cout<<"Enter asmark1 :";
 		cin>>asmark1;
-		cout<<"Enter asmark2 :"<<endl;
+		cout<<"Enter asmark2 :";
 		cin>>asmark2;
 		tint=intm1+intm2+asmark1+asmark2;
-		cout<<"Enter endsem marks :"<<endl;
+		cout<<"Enter endsem marks :";
 		cin>>endsem;
 		total=tint+endsem;
 		compute();
@@ -67,9 +67,9 @@ class gradeCard{
 	{
 		if(total>90)
 			gpa=10;
-		if(80<total<90)
+		else if(80<total&&total<90)
 			gpa=9;
-		if(70<total<80)
+		if(70<total&&total<80)
 			gpa=8;
 	}
 	void viewGradecard()
